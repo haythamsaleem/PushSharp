4dfdb5fa78ea7e4cb4a4652d3b03a30edfc87ea2
Haytham Alabdelqader <haythamsaleem2010@gmail.com>
2016-03-23 14:41:54 +0300

    Update GcmServiceConnection.cs
    Option to use a Proxy when connecting to Android

--- PushSharp.Google/GcmServiceConnection.cs
+++ PushSharp.Google/GcmServiceConnection.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Threading.Tasks;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -8,12 +8,15 @@ using PushSharp.Core;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
+using System.Net.Sockets;
+using System.Diagnostics;
+using System.IO;
 
 namespace PushSharp.Google
 {
     public class GcmServiceConnectionFactory : IServiceConnectionFactory<GcmNotification>
     {
-        public GcmServiceConnectionFactory (GcmConfiguration configuration)
+        public GcmServiceConnectionFactory(GcmConfiguration configuration)
         {
             Configuration = configuration;
         }
@@ -22,83 +25,96 @@ namespace PushSharp.Google
 
         public IServiceConnection<GcmNotification> Create()
         {
-            return new GcmServiceConnection (Configuration);
+            return new GcmServiceConnection(Configuration);
         }
     }
 
     public class GcmServiceBroker : ServiceBroker<GcmNotification>
     {
-        public GcmServiceBroker (GcmConfiguration configuration) : base (new GcmServiceConnectionFactory (configuration))
+        public GcmServiceBroker(GcmConfiguration configuration) : base(new GcmServiceConnectionFactory(configuration))
         {
         }
     }
 
     public class GcmServiceConnection : IServiceConnection<GcmNotification>
     {
-        public GcmServiceConnection (GcmConfiguration configuration)
+        public GcmServiceConnection(GcmConfiguration configuration)
         {
-            Configuration = configuration;
-            http = new HttpClient ();
 
-            http.DefaultRequestHeaders.UserAgent.Clear ();
-            http.DefaultRequestHeaders.UserAgent.Add (new ProductInfoHeaderValue ("PushSharp", "3.0"));
-            http.DefaultRequestHeaders.TryAddWithoutValidation ("Authorization", "key=" + Configuration.SenderAuthToken);
+
+            Configuration = configuration;
+            if (Configuration.UseProxy)
+            {
+                http = getClientViaHTTPProxy(Configuration.ProxyHost, Configuration.ProxyPort);
+            }
+            else
+            {
+                http = new HttpClient();
+            }
+            http.DefaultRequestHeaders.UserAgent.Clear();
+            http.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("PushSharp", "3.0"));
+            http.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", "key=" + Configuration.SenderAuthToken);
         }
 
         public GcmConfiguration Configuration { get; private set; }
 
         readonly HttpClient http;
 
-        public async Task Send (GcmNotification notification)
+        public async Task Send(GcmNotification notification)
         {
-            var json = notification.GetJson ();
+            var json = notification.GetJson();
 
-            var content = new StringContent (json, System.Text.Encoding.UTF8, "application/json");
+            var content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
 
-            var response = await http.PostAsync (Configuration.GcmUrl, content);
+            var response = await http.PostAsync(Configuration.GcmUrl, content);
 
-            if (response.IsSuccessStatusCode) {
-                await processResponseOk (response, notification);
-            } else {
-                var body = await response.Content.ReadAsStringAsync ();
-                processResponseError (response, notification);
+            if (response.IsSuccessStatusCode)
+            {
+                await processResponseOk(response, notification);
+            }
+            else {
+                var body = await response.Content.ReadAsStringAsync();
+                processResponseError(response, notification);
             }
         }
 
-        async Task processResponseOk (HttpResponseMessage httpResponse, GcmNotification notification)
+        async Task processResponseOk(HttpResponseMessage httpResponse, GcmNotification notification)
         {
-            var multicastResult = new GcmMulticastResultException ();
+            var multicastResult = new GcmMulticastResultException();
 
-            var result = new GcmResponse () {
+            var result = new GcmResponse()
+            {
                 ResponseCode = GcmResponseCode.Ok,
                 OriginalNotification = notification
             };
 
-            var str = await httpResponse.Content.ReadAsStringAsync ();
-            var json = JObject.Parse (str); 
+            var str = await httpResponse.Content.ReadAsStringAsync();
+            var json = JObject.Parse(str);
 
-            result.NumberOfCanonicalIds = json.Value<long> ("canonical_ids");
-            result.NumberOfFailures = json.Value<long> ("failure");
-            result.NumberOfSuccesses = json.Value<long> ("success");
+            result.NumberOfCanonicalIds = json.Value<long>("canonical_ids");
+            result.NumberOfFailures = json.Value<long>("failure");
+            result.NumberOfSuccesses = json.Value<long>("success");
 
-            var jsonResults = json ["results"] as JArray ?? new JArray ();
+            var jsonResults = json["results"] as JArray ?? new JArray();
 
-            foreach (var r in jsonResults) {
-                var msgResult = new GcmMessageResult ();
+            foreach (var r in jsonResults)
+            {
+                var msgResult = new GcmMessageResult();
 
-                msgResult.MessageId = r.Value<string> ("message_id");
-                msgResult.CanonicalRegistrationId = r.Value<string> ("registration_id");
+                msgResult.MessageId = r.Value<string>("message_id");
+                msgResult.CanonicalRegistrationId = r.Value<string>("registration_id");
                 msgResult.ResponseStatus = GcmResponseStatus.Ok;
 
-                if (!string.IsNullOrEmpty (msgResult.CanonicalRegistrationId))
+                if (!string.IsNullOrEmpty(msgResult.CanonicalRegistrationId))
                     msgResult.ResponseStatus = GcmResponseStatus.CanonicalRegistrationId;
-                else if (r ["error"] != null) {
-                    var err = r.Value<string> ("error") ?? "";
+                else if (r["error"] != null)
+                {
+                    var err = r.Value<string>("error") ?? "";
 
-                    msgResult.ResponseStatus = GetGcmResponseStatus (err);
+                    msgResult.ResponseStatus = GetGcmResponseStatus(err);
                 }
 
-                result.Results.Add (msgResult);
+                result.Results.Add(msgResult);
             }
 
             int index = 0;
@@ -106,48 +122,59 @@ namespace PushSharp.Google
             //Loop through every result in the response
             // We will raise events for each individual result so that the consumer of the library
             // can deal with individual registrationid's for the notification
-            foreach (var r in result.Results) {
-                var singleResultNotification = GcmNotification.ForSingleResult (result, index);
+            foreach (var r in result.Results)
+            {
+                var singleResultNotification = GcmNotification.ForSingleResult(result, index);
 
                 singleResultNotification.MessageId = r.MessageId;
 
-                if (r.ResponseStatus == GcmResponseStatus.Ok) { // Success
-                    multicastResult.Succeeded.Add (singleResultNotification);
-                } else if (r.ResponseStatus == GcmResponseStatus.CanonicalRegistrationId) { //Need to swap reg id's
+                if (r.ResponseStatus == GcmResponseStatus.Ok)
+                { // Success
+                    multicastResult.Succeeded.Add(singleResultNotification);
+                }
+                else if (r.ResponseStatus == GcmResponseStatus.CanonicalRegistrationId)
+                { //Need to swap reg id's
                     //Swap Registrations Id's
                     var newRegistrationId = r.CanonicalRegistrationId;
                     var oldRegistrationId = string.Empty;
 
                     if (singleResultNotification.RegistrationIds != null && singleResultNotification.RegistrationIds.Count > 0)
-                        oldRegistrationId = singleResultNotification.RegistrationIds [0];
+                        oldRegistrationId = singleResultNotification.RegistrationIds[0];
 
-                    multicastResult.Failed.Add (singleResultNotification, new DeviceSubscriptonExpiredException {
+                    multicastResult.Failed.Add(singleResultNotification, new DeviceSubscriptonExpiredException
+                    {
                         OldSubscriptionId = oldRegistrationId,
                         NewSubscriptionId = newRegistrationId
                     });
-                } else if (r.ResponseStatus == GcmResponseStatus.Unavailable) { // Unavailable
-                    multicastResult.Failed.Add (singleResultNotification, new GcmConnectionException ("Unavailable Response Status"));
-                } else if (r.ResponseStatus == GcmResponseStatus.NotRegistered) { //Bad registration Id
+                }
+                else if (r.ResponseStatus == GcmResponseStatus.Unavailable)
+                { // Unavailable
+                    multicastResult.Failed.Add(singleResultNotification, new GcmConnectionException("Unavailable Response Status"));
+                }
+                else if (r.ResponseStatus == GcmResponseStatus.NotRegistered)
+                { //Bad registration Id
                     var oldRegistrationId = string.Empty;
 
                     if (singleResultNotification.RegistrationIds != null && singleResultNotification.RegistrationIds.Count > 0)
-                        oldRegistrationId = singleResultNotification.RegistrationIds [0];
+                        oldRegistrationId = singleResultNotification.RegistrationIds[0];
 
-                    multicastResult.Failed.Add (singleResultNotification, new DeviceSubscriptonExpiredException { OldSubscriptionId = oldRegistrationId });
-                } else {
-                    multicastResult.Failed.Add (singleResultNotification, new GcmConnectionException ("Unknown Failure: " + r.ResponseStatus));
+                    multicastResult.Failed.Add(singleResultNotification, new DeviceSubscriptonExpiredException { OldSubscriptionId = oldRegistrationId });
+                }
+                else {
+                    multicastResult.Failed.Add(singleResultNotification, new GcmConnectionException("Unknown Failure: " + r.ResponseStatus));
                 }
 
                 index++;
             }
 
             // If we only have 1 total result, it is not *multicast*, 
-            if (multicastResult.Succeeded.Count + multicastResult.Failed.Count == 1) {
+            if (multicastResult.Succeeded.Count + multicastResult.Failed.Count == 1)
+            {
                 // If not multicast, and succeeded, don't throw any errors!
                 if (multicastResult.Succeeded.Count == 1)
                     return;
                 // Otherwise, throw the one single failure we must have
-                throw multicastResult.Failed.First ().Value;
+                throw multicastResult.Failed.First().Value;
             }
 
             // If we get here, we must have had a multicast message
@@ -156,41 +183,76 @@ namespace PushSharp.Google
                 throw multicastResult;
         }
 
-        void processResponseError (HttpResponseMessage httpResponse, GcmNotification notification)
+        void processResponseError(HttpResponseMessage httpResponse, GcmNotification notification)
         {
             //401 bad auth token
             if (httpResponse.StatusCode == HttpStatusCode.Unauthorized)
-                throw new UnauthorizedAccessException ("GCM Authorization Failed");
+                throw new UnauthorizedAccessException("GCM Authorization Failed");
 
             if (httpResponse.StatusCode == HttpStatusCode.BadRequest)
-                throw new GcmConnectionException ("HTTP 400 Bad Request");
+                throw new GcmConnectionException("HTTP 400 Bad Request");
 
-            if ((int)httpResponse.StatusCode >= 500 && (int)httpResponse.StatusCode < 600) {
+            if ((int)httpResponse.StatusCode >= 500 && (int)httpResponse.StatusCode < 600)
+            {
                 //First try grabbing the retry-after header and parsing it.
                 var retryAfterHeader = httpResponse.Headers.RetryAfter;
 
-                if (retryAfterHeader != null && retryAfterHeader.Delta.HasValue) {
+                if (retryAfterHeader != null && retryAfterHeader.Delta.HasValue)
+                {
                     var retryAfter = retryAfterHeader.Delta.Value;
-                    throw new RetryAfterException ("GCM Requested Backoff", DateTime.UtcNow + retryAfter);
-                }                  
+                    throw new RetryAfterException("GCM Requested Backoff", DateTime.UtcNow + retryAfter);
+                }
             }
 
-            throw new GcmConnectionException ("GCM HTTP Error: " + httpResponse.StatusCode);           
+            throw new GcmConnectionException("GCM HTTP Error: " + httpResponse.StatusCode);
         }
 
-        static GcmResponseStatus GetGcmResponseStatus (string str)
+        static GcmResponseStatus GetGcmResponseStatus(string str)
         {
             var enumType = typeof(GcmResponseStatus);
 
-            foreach (var name in Enum.GetNames (enumType)) {
-                var enumMemberAttribute = ((EnumMemberAttribute[])enumType.GetField (name).GetCustomAttributes (typeof(EnumMemberAttribute), true)).Single ();
+            foreach (var name in Enum.GetNames(enumType))
+            {
+                var enumMemberAttribute = ((EnumMemberAttribute[])enumType.GetField(name).GetCustomAttributes(typeof(EnumMemberAttribute), true)).Single();
 
-                if (enumMemberAttribute.Value.Equals (str, StringComparison.InvariantCultureIgnoreCase))
-                    return (GcmResponseStatus)Enum.Parse (enumType, name);
+                if (enumMemberAttribute.Value.Equals(str, StringComparison.InvariantCultureIgnoreCase))
+                    return (GcmResponseStatus)Enum.Parse(enumType, name);
             }
 
             //Default
             return GcmResponseStatus.Error;
+        }
+        private HttpClient getClientViaHTTPProxy(string httpProxyHost, int httpProxyPort)
+        {
+            HttpClient client = null;
+            try
+            {
+                string proxyUri =
+                    string.Format("{0}:{1}", httpProxyHost, httpProxyPort);
+
+                WebProxy proxy = new WebProxy(proxyUri, false)
+                {
+                    UseDefaultCredentials = true,
+                };
+
+                // Now create a client handler which uses that proxy
+
+
+                HttpClientHandler httpClientHandler = new HttpClientHandler()
+                {
+                    Proxy = proxy,
+                    PreAuthenticate = false,
+                    UseDefaultCredentials = true,
+                };
+                client = new HttpClient(httpClientHandler);
+            }
+            catch (Exception ex)
+            {
+                client = null;
+                Log.Error(ex.Message);
+                throw;
+            }
+            return client;
         }
     }
 }
